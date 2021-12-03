da45e77439ea4fecac5687e88ae8fc9d3d6dcb7b
xzqu1t.xyz <62037864+xzqu1t@users.noreply.github.com>
2021-12-03 22:04:12 +0300

    4

--- promoyandex/calcpromo.js
+++ promoyandex/calcpromo.js
@@ -1,8 +1,8 @@
 function promo() {
-    var num1,num2,num3,num4;
+    var num1, num2, num3, num4;
     num1 = document.getElementById('n1').value;
     num1 = parseInt(num1);
-    
+
     if (num1 * 0.1 <= 200) num4 = 200;
     if (num1 * 0.1 > 200) num3 = Math.ceil(num1 * 0.1 / 100) * 100;
     if (num3 <= 5000) num4 = num3;
