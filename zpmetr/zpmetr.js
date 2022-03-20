8b82ac6eb1d2bd53f3352363784ef90211b0051f
xzqu1t.xyz <62037864+xzqu1t@users.noreply.github.com>
2022-03-20 21:14:20 +0300

    update + buttons

--- zpmetr/zpmetr.js
+++ zpmetr/zpmetr.js
@@ -5,64 +5,61 @@ var stop = document.getElementById('stp');
 var reset = document.getElementById('rst');
 var sec = 0;
 var min = 0;
-var hrs= 0;
-var wec = 0;
-var win = 0;
-var wrs = 0;
+var hrs = 0;
+var zp = 0;
 var t;
-var w;
 
 function tick(){
     sec++;
     if (sec >= 60) {
         sec = 0;
-        min = (min + 2.5);
-    }
-}
-function tick1(){
-    wec++;
-    if (wec >= 60) {
-        wec = 0;
-        win++;
-        if (win >= 60) {
-            win = 0;
-            wrs++;
+        min++;
+        if (min >= 60) {
+            min = 0;
+            hrs++;
         }
     }
 }
 function add() {
     tick();
-    h1.textContent = (min);
+    h1.textContent = (hrs > 9 ? hrs : "0" + hrs) 
+        	 + ":" + (min > 9 ? min : "0" + min)
+       		 + ":" + (sec > 9 ? sec : "0" + sec);
+    zp = (hrs * 150 + min * 2.5);
+    h2.textContent = zp;
     timer();
 }
-function add2() {
-    tick1();
-    h2.textContent = (wrs > 9 ? wrs : "0" + wrs) 
-       + ":" + (win > 9 ? win : "0" + win)
-       + ":" + (wec > 9 ? wec : "0" + wec);
-    timer2();
+
+function plushrsz() {
+    hrs++;
+    h1.textContent = (hrs > 9 ? hrs : "0" + hrs) 
+    + ":" + (min > 9 ? min : "0" + min)
+    + ":" + (sec > 9 ? sec : "0" + sec);
+    zp = (hrs * 150 + min * 2.5);
+    h2.textContent = zp;
 }
+
+function plusminx() {
+    min++;
+    h1.textContent = (hrs > 9 ? hrs : "0" + hrs) 
+    + ":" + (min > 9 ? min : "0" + min)
+    + ":" + (sec > 9 ? sec : "0" + sec);
+    zp = (hrs * 150 + min * 2.5);
+    h2.textContent = zp;
+}
+
 function timer() {
-    t = setTimeout(add, 1000);
+    t = setTimeout(add, 50);
 }
-function timer2() {
-    w = setTimeout(add2, 1000);
-}
-timer();
-timer2();
+
 start.onclick = timer;
-start.onclick = timer2;
 stop.onclick = function() {
     clearTimeout(t);
 }
-stop.onclick = function() {
-    clearTimeout(w);
-}
 reset.onclick = function() {
-    h1.textContent = "0";
-    min = 0; sec = 0;
+    h1.textContent = "00:00:00";
+    h2.textContent = "0";
+    sec = 0; min = 0; hrs = 0;
+    clearTimeout(t);
 }
-reset.onclick = function() {
-    h2.textContent = "00:00:00";
-    wrs = 0; win = 0; wec = 0; 
-}
+
